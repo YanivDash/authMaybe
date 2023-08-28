5f2b79366b691969ba239e0457756f542c6c1851
vivek Latwal <vivek@coadjute.com>
2023-08-28 21:41:22 +0530

    second commit adding website

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 // used to enable Cross-Origin Resource Sharing (CORS),
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://comfy-lily-16b571.netlify.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })
